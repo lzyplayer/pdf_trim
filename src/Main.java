f174ec43911386d73a12d36d5dd1a0cf0c101ccd
lzyplayer <292442545@qq.com>
2018-04-19 20:44:09 +0800

    a test on intelij_direct_github_push_commit SSH_checked

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        File file = new File("tar.txt");
+        try {
+            Scanner scanner =new Scanner(file);
+//            String result = null;
+            while (scanner.hasNext()){
+                System.out.print(scanner.nextLine()+" ");
+//                scanner.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
